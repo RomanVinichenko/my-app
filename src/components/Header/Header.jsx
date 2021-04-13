b04a7743929d87f6d752afab85b37feb747491c0
roman <elik@example.com>
2021-04-13 20:32:03 +0300

    App

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Header.module.css';
+
+const Header = () =>{
+return <header className={s.header}>
+        <img src='https://images.vectorhq.com/images/previews/3c5/nike-logo-2-psd-468142.png'></img>
+      </header>
+}
+
+export default Header;
---
+++ src/components/Profile/Post/MyPosts.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import s from './MyPosts.module.css';
+import Post from './Post';
+
+const MyPosts = () =>{
+return ( 
+<div className={s.content}>
+   <div>
+      my posts
+     <div>
+        <textarea></textarea>
+        <button>Add post</button>
+        
+     </div>
+     <div className={s.posts}>
+        <Post/>
+        <Post/>
+        <Post/>
+     </div>
+     </div>
+</div>
+)
+}
+
+export default MyPosts;
