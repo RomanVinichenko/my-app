48f30393f53f587afbf6579c2d435b8523739a75
roman <elik@example.com>
2021-04-14 21:13:23 +0300

    Dialogs

--- src/components/Dialogs/index.jsx
+++ src/components/Dialogs/index.jsx
@@ -4,8 +4,28 @@ import styles from './style.module.scss';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={styles ['dialogs']}>
+            <div className="dialogs-items">
+               <div className="dialog">
+                  Maks
+               </div>
+               <div className="dialog">
+                  Olja
+               </div>
+               <div className="dialog">
+                  Ilya
+               </div>
+               <div className="dialog">
+                  Ksu
+               </div>
+            </div>
+            <div className="messages">
+               <div>
+                 <div className="message">React!</div>
+                 <div className="message">Html?</div>
+                 <div className="message">Css%</div>
+               </div>
+            </div>
         </div>
     )
 }
